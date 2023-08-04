7072e2bc72f963f3d45b5bdb9cdf0813ed24c164
Josh Totten <josh.totten8@gmail.com>
2023-08-04 13:12:09 -0400

    Completed add node

--- Workspace.c
+++ Workspace.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-//Create Node
+//Node Structure
 typedef struct Node
 {
     int data;
     struct Node *next;
 }Node;
 
-int main (int argc, char * argv[]){
 
+
+//Create Node
+Node * createNode(void){
+    Node * temp = (Node*)malloc(sizeof(Node));
+    temp->next = NULL;
+    return temp;
+}
+
+//Add node to list
+Node* push(Node* head, int data){
+    Node *temp = createNode();
+    Node *p = createNode();
+    temp->data = data;
+    if(head == NULL){
+        head = temp;
+    }
+    else{
+        p = head;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        p->next = temp;
+    }
+    return head;
+}
+
+int main (int argc, char * argv[]){
+    Node* head = NULL;
+    head = push(head,2);
+    printf("%d", head->data);
     return 0;
 }
 
